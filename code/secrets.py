c0b69a7dd882f0c67b23956b82c0449cdde34b4c
saiph <saiphcita@gmail.com>
2015-10-05 18:51:01 -0400

    code to store data of bots

--- code/secrets.py
+++ code/secrets.py
@@ -1,8 +1,47 @@
-C_KEY = "T48gHlgfSgEFuOS76N2gy7bj9"  
-C_SECRET = "7ntJ1xPdSxTjR2bvKVMO0flCvn0xM9AHrhg0DzY77iGnbB6Rzw"  
-A_TOKEN = "3754637839-k4If4g7Ti4a0Ixem8clzkxcBPBa3ASPRgNGMajx"  
-A_TOKEN_SECRET = "0nXsjBausDjSouYLuS3VjiAZgZEDJFZYiuvqCOImhcKxQ"  
+#bot
+#C_KEY = "T48gHlgfSgEFuOS76N2gy7bj9"  
+#C_SECRET = "7ntJ1xPdSxTjR2bvKVMO0flCvn0xM9AHrhg0DzY77iGnbB6Rzw"  
+#A_TOKEN = "3754637839-BcqriLQ0xS9EJzqzkz5KKZg94lsGmY8kqS2S6Rb"  
+#A_TOKEN_SECRET = "sI9YoXx6zqOdlOFgyhPTVKZ7hgqAPGwNza1FKAdyJL1pt" 
 
+#lady
+C_KEY = "VQmFOYXWmEbhbzOE8cpeTg8Rr"  
+C_SECRET = "yipVsherO4DuUpXtdryCA4JhKdImqJFQcLwWN63UgdXID2bkze"  
+A_TOKEN = "3754491014-p9V41yXNgZ3fwdzlkhPqJzhjmRemcYJzzCYsyMT"  
+A_TOKEN_SECRET = "PorWL4M8x2KvzooBu2TG7CPNOeT4L7aDoh8xFdY5zvoAu"  
+
+
+
+#consumer Key (API Key) T48gHlgfSgEFuOS76N2gy7bj9
+#Consumer Secret (API Secret) 7ntJ1xPdSxTjR2bvKVMO0flCvn0xM9AHrhg0DzY77iGnbB6Rzw
+ #Access Token 3754637839-BcqriLQ0xS9EJzqzkz5KKZg94lsGmY8kqS2S6Rb
+#Access Token Secret sI9YoXx6zqOdlOFgyhPTVKZ7hgqAPGwNza1FKAdyJL1pt  
+
+
+
+
+
+#consumer_key = 'T48gHlgfSgEFuOS76N2gy7bj9'
+##consumer_secret = '7ntJ1xPdSxTjR2bvKVMO0flCvn0xM9AHrhg0DzY77iGnbB6Rzw'
+#access_token = '3754637839-BcqriLQ0xS9EJzqzkz5KKZg94lsGmY8kqS2S6Rb'
+#access_token_secret = 'sI9YoXx6zqOdlOFgyhPTVKZ7hgqAPGwNza1FKAdyJL1pt'
+
+
+
+ #Consumer Key (API Key) VQmFOYXWmEbhbzOE8cpeTg8Rr
+#Consumer Secret (API Secret) yipVsherO4DuUpXtdryCA4JhKdImqJFQcLwWN63UgdXID2bkze
+ #Access Token 3754491014-p9V41yXNgZ3fwdzlkhPqJzhjmRemcYJzzCYsyMT
+#Access Token Secret PorWL4M8x2KvzooBu2TG7CPNOeT4L7aDoh8xFdY5zvoAu 
+
+
+#BotMujeres
+#consumer_key = 'T48gHlgfSgEFuOS76N2gy7bj9'
+##consumer_secret = '7ntJ1xPdSxTjR2bvKVMO0flCvn0xM9AHrhg0DzY77iGnbB6Rzw'
+#access_token = '3754637839-BcqriLQ0xS9EJzqzkz5KKZg94lsGmY8kqS2S6Rb'
+#access_token_secret = 'sI9YoXx6zqOdlOFgyhPTVKZ7hgqAPGwNza1FKAdyJL1pt'
+
+#Access Token 3754637839-BcqriLQ0xS9EJzqzkz5KKZg94lsGmY8kqS2S6Rb
+#Access Token Secret sI9YoXx6zqOdlOFgyhPTVKZ7hgqAPGwNza1FKAdyJL1pt 
 
 #Consumer Key (API Key)	T48gHlgfSgEFuOS76N2gy7bj9
 #Consumer Secret (API Secret)	7ntJ1xPdSxTjR2bvKVMO0flCvn0xM9AHrhg0DzY77iGnbB6Rzw
--- code/unfollowers2.py
+++ code/unfollowers2.py
@@ -29,21 +29,38 @@ def unFollowSuckers(SCREEN_NAME):
 	followers = api.followers_ids(SCREEN_NAME)
 	friends = api.friends_ids(SCREEN_NAME)
 	latestPeopleAlreadyFollowed=pickle.load(open("latestPeopleAlreadyFollowed_"+SCREEN_NAME+".p","rb"))
+	#latestPeopleAlreadyFollowed={}
  	print "Num followers:"+str(len(followers))
  	print "Num freid:"+str(len(followers))
 	for f in friends:
 		if f not in followers:
 			print "Unfollow {0}?".format(api.get_user(f).screen_name)
 			#if raw_input("Y/N?") == 'y' or 'Y':
-			#
-			latestPeopleAlreadyFollowed[f]=0
+			userName=api.get_user(f).screen_name
+			latestPeopleAlreadyFollowed[userName]=0
 			pickle.dump(latestPeopleAlreadyFollowed, open("latestPeopleAlreadyFollowed_"+SCREEN_NAME+".p", "wb"))
 			api.destroy_friendship(f)
+	FILE=open("peopleOnceFollowed_"+SCREEN_NAME+".csv",'w')
+	for p in latestPeopleAlreadyFollowed:
+		FILE.write(str(p)+"\n")
+	FILE.close()
 
 #l = StdOutListener()
-SCREEN_NAME="MujeresFemBot"
-api,auth=authenticateBot()
-getPeopleToFollow(api)
+#Mujeres__Fem
+#SCREEN_NAME="MujeresFemBot"
+SCREEN_NAME="Mujeres__Fem"
+unFollowSuckers(SCREEN_NAME)
+latestPeopleAlreadyFollowed=pickle.load(open("latestPeopleAlreadyFollowed_"+SCREEN_NAME+".p", "rb"))
+FILE=open("peopleOnceFollowed_"+SCREEN_NAME+".csv",'w')
+for p in latestPeopleAlreadyFollowed:
+	FILE.write(str(p)+"\n")
+FILE.close()
+#unFollowSuckers(SCREEN_NAME)
+
+
+
+#api,auth=authenticateBot()
+#getPeopleToFollow(api)
 #unFollowSuckers(SCREEN_NAME)
 #stream = tweepy.Stream(auth, l)
 
